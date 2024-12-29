bd5a7d94e22a42badda41372c12ac563025b652d
Vinicius-O-Ferraz <vinicius.f.ferraz.bsi@gmail.com>
2024-12-28 21:42:19 -0300

    initial code

---
+++ index.js
@@ -0,0 +1,71 @@
+const http = require('http')
+const fs = require('fs')
+const pokemonsKanto = [
+    "Bulbasaur", "Ivysaur", "Venusaur", "Charmander", "Charmeleon", "Charizard",
+    "Squirtle", "Wartortle", "Blastoise", "Caterpie", "Metapod", "Butterfree",
+    "Weedle", "Kakuna", "Beedrill", "Pidgey", "Pidgeotto", "Pidgeot",
+    "Rattata", "Raticate", "Spearow", "Fearow", "Ekans", "Arbok",
+    "Pikachu", "Raichu", "Sandshrew", "Sandslash", "Nidorina",
+    "Nidoqueen", "Nidoran", "Nidorino", "Nidoking", "Clefairy", "Clefable",
+    "Vulpix", "Ninetales", "Jigglypuff", "Wigglytuff", "Zubat", "Golbat",
+    "Oddish", "Gloom", "Vileplume", "Paras", "Parasect", "Venonat",
+    "Venomoth", "Diglett", "Dugtrio", "Meowth", "Persian", "Psyduck",
+    "Golduck", "Mankey", "Primeape", "Growlithe", "Arcanine", "Poliwag",
+    "Poliwhirl", "Poliwrath", "Abra", "Kadabra", "Alakazam", "Machop",
+    "Machoke", "Machamp", "Bellsprout", "Weepinbell", "Victreebel",
+    "Tentacool", "Tentacruel", "Geodude", "Graveler", "Golem", "Ponyta",
+    "Rapidash", "Slowpoke", "Slowbro", "Magnemite", "Magneton", "Farfetch'd",
+    "Doduo", "Dodrio", "Seel", "Dewgong", "Grimer", "Muk", "Shellder",
+    "Cloyster", "Gastly", "Haunter", "Gengar", "Onix", "Drowzee", "Hypno",
+    "Krabby", "Kingler", "Voltorb", "Electrode", "Exeggcute", "Exeggutor",
+    "Cubone", "Marowak", "Hitmonlee", "Hitmonchan", "Lickitung", "Koffing",
+    "Weezing", "Rhyhorn", "Rhydon", "Chansey", "Tangela", "Kangaskhan",
+    "Horsea", "Seadra", "Goldeen", "Seaking", "Staryu", "Starmie", "Mr. Mime",
+    "Scyther", "Jynx", "Electabuzz", "Magmar", "Pinsir", "Tauros", "Magikarp",
+    "Gyarados", "Lapras", "Ditto", "Eevee", "Vaporeon", "Jolteon", "Flareon",
+    "Porygon", "Omanyte", "Omastar", "Kabuto", "Kabutops", "Aerodactyl",
+    "Snorlax", "Articuno", "Zapdos", "Moltres", "Dratini", "Dragonair",
+    "Dragonite", "Mewtwo", "Mew"
+  ];
+
+let score = 0
+let discovered = []
+
+console.log(pokemonsKanto)
+
+const port = 3000
+const server = http.createServer((req,res)=>{
+
+    const urlInfo = require("url").parse(req.url, true)
+    const name = urlInfo.query.name
+
+    if(!name){
+        fs.readFile ('index.html',function(err,data){
+            res.writeHead(200,{'Content-Type':'text/html'})
+            res.write(data)
+            return res.end()
+           })
+    }else{
+
+        if(pokemonsKanto.includes(name) && !discovered.includes(name)){
+            score = score + 1
+            discovered.push(name)
+            console.log(`O seu score é ${score}`)
+            console.log(discovered)
+        }
+
+    const nameNewline = name + "\r\n"
+
+        fs.appendFile("arquivo.txt",nameNewline,function(err, data){
+            res.writeHead(302,{
+                Location:'/',
+        })
+        return res.end()
+})
+}
+})
+
+server.listen(port,()=>{
+    // console.log(`Servidor rodando na porta ${port}`)
+    console.log()
+})
